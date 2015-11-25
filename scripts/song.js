81023245b73518dc75fac823cf983ddaeda40f6b
Steven Davies <el_loserio@ntlworld.com>
2015-11-25 22:07:13 +0000

    Added more songs

--- scripts/song.js
+++ scripts/song.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
         'Joy Division – Love Will Tear Us Apart',
         'Gram Parsons – Streets Of Baltimore',
         'Fleetwood Mac – Go Your Own Way',
+        'Outkast – Hey Ya!',
+        'Kelis – Caught Out There',
     ];
     $('#first-dance').text(songs[Math.floor(Math.random() * songs.length)]);
 });
